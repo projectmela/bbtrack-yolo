f39b764f4f56f823d1468a44e2e892589789960a
Joilence <14588641+Joilence@users.noreply.github.com>
2023-12-26 20:06:34 +0100

    [snapshot] eval for ontology

--- yolov8_eval.py
+++ yolov8_eval.py
@@ -1,4 +1,5 @@
 import json
+import os
 
 from ultralytics import YOLO
 
@@ -7,35 +8,35 @@ from utility import cur_dt_str
 model_paths = [
     # /scratch/cs/css-llm/jonathan/localcode/bb-yolo/models/models_202311122_gd
     # /scratch/cs/css-llm/jonathan/localcode/bb-yolo/models/models_20231117_mc
-    # models 20231117 mc
-    "/scratch/cs/css-llm/jonathan/localcode/bb-yolo/models/models_20231117_mc/d=mc_dtc2023_m=yolov8x_imgsz=2560_bs=4_20231118-005142/weights/best.pt",
-    "/scratch/cs/css-llm/jonathan/localcode/bb-yolo/models/models_20231117_mc/d=mc_dtc2023_m=yolov8n_imgsz=2560_bs=4_20231118-005145/weights/best.pt",
-    "/scratch/cs/css-llm/jonathan/localcode/bb-yolo/models/models_20231117_mc/d=mc_dtc2023_m=yolov8x_imgsz=1280_bs=16_20231118-012653/weights/best.pt",
-    "/scratch/cs/css-llm/jonathan/localcode/bb-yolo/models/models_20231117_mc/d=mc_dtc2023_m=yolov8m_imgsz=2560_bs=4_20231118-005146/weights/best.pt",
-    "/scratch/cs/css-llm/jonathan/localcode/bb-yolo/models/models_20231117_mc/d=mc_dtc2023_m=yolov8n_imgsz=1280_bs=16_20231118-014426/weights/best.pt",
-    "/scratch/cs/css-llm/jonathan/localcode/bb-yolo/models/models_20231117_mc/d=mc_dtc2023_m=yolov8m_imgsz=1280_bs=16_20231118-014225/weights/best.pt",
-    "/scratch/cs/css-llm/jonathan/localcode/bb-yolo/models/models_20231117_mc/d=mc_dtc2023_m=yolov8x_imgsz=5472_bs=1_20231118-005137/weights/best.pt",
-    "/scratch/cs/css-llm/jonathan/localcode/bb-yolo/models/models_20231117_mc/d=mc_dtc2023_m=yolov8m_imgsz=5472_bs=1_20231118-005143/weights/best.pt",
-    "/scratch/cs/css-llm/jonathan/localcode/bb-yolo/models/models_20231117_mc/d=mc_dtc2023_m=yolov8n_imgsz=5472_bs=1_20231118-005143/weights/best.pt",
-
-    # models 20231122 gd
-    "/scratch/cs/css-llm/jonathan/localcode/bb-yolo/models/models_202311122_gd/d=gd_dtc2023_m=yolov8m_imgsz=2560_bs=4_20231122-050555/weights/best.pt",
-    "/scratch/cs/css-llm/jonathan/localcode/bb-yolo/models/models_202311122_gd/d=gd_dtc2023_m=yolov8x_imgsz=2560_bs=4_20231122-040301/weights/best.pt",
-    "/scratch/cs/css-llm/jonathan/localcode/bb-yolo/models/models_202311122_gd/d=gd_dtc2023_m=yolov8n_imgsz=2560_bs=4_20231122-054018/weights/best.pt",
-    "/scratch/cs/css-llm/jonathan/localcode/bb-yolo/models/models_202311122_gd/d=gd_dtc2023_m=yolov8n_imgsz=1280_bs=16_20231122-073256/weights/best.pt",
-    "/scratch/cs/css-llm/jonathan/localcode/bb-yolo/models/models_202311122_gd/d=gd_dtc2023_m=yolov8m_imgsz=1280_bs=16_20231122-061030/weights/best.pt",
-    "/scratch/cs/css-llm/jonathan/localcode/bb-yolo/models/models_202311122_gd/d=gd_dtc2023_m=yolov8x_imgsz=1280_bs=16_20231122-054203/weights/best.pt",
-    "/scratch/cs/css-llm/jonathan/localcode/bb-yolo/models/models_202311122_gd/d=gd_dtc2023_m=yolov8n_imgsz=5472_bs=1_20231122-035129/weights/best.pt",
-    "/scratch/cs/css-llm/jonathan/localcode/bb-yolo/models/models_202311122_gd/d=gd_dtc2023_m=yolov8m_imgsz=5472_bs=1_20231122-035009/weights/best.pt",
-    "/scratch/cs/css-llm/jonathan/localcode/bb-yolo/models/models_202311122_gd/d=gd_dtc2023_m=yolov8x_imgsz=5472_bs=1_20231122-024757/weights/best.pt",
+    # models_20231219_ontology
+    "/scratch/cs/css-llm/jonathan/localcode/bb-yolo/models/models_20231219_ontology/d=mc_dtc2023_gd_o=gd_m=yolov8m_imgsz=5472_bs=1_20231219-061150",
+    "/scratch/cs/css-llm/jonathan/localcode/bb-yolo/models/models_20231219_ontology/d=mc_dtc2023_gd_o=gd_m=yolov8x_imgsz=5472_bs=1_20231219-055807",
+    "/scratch/cs/css-llm/jonathan/localcode/bb-yolo/models/models_20231219_ontology/d=mc_dtc2023_all-mc_o=all-mc_m=yolov8x_imgsz=5472_bs=1_20231219-055807",
+    "/scratch/cs/css-llm/jonathan/localcode/bb-yolo/models/models_20231219_ontology/d=mc_dtc2023_all-mc_o=all-mc_m=yolov8m_imgsz=5472_bs=1_20231219-055807",
+    "/scratch/cs/css-llm/jonathan/localcode/bb-yolo/models/models_20231219_ontology/d=mc_dtc2023_gd-shadow_o=gd-shadow_m=yolov8x_imgsz=5472_bs=1_20231219-054002",
+    "/scratch/cs/css-llm/jonathan/localcode/bb-yolo/models/models_20231219_ontology/d=mc_dtc2023_gd-drone-bird_o=gd-drone-bird_m=yolov8x_imgsz=5472_bs=1_20231219-054003",
+    "/scratch/cs/css-llm/jonathan/localcode/bb-yolo/models/models_20231219_ontology/d=mc_dtc2023_rm-unknown_o=rm-unknown_m=yolov8x_imgsz=5472_bs=1_20231219-054002",
+    "/scratch/cs/css-llm/jonathan/localcode/bb-yolo/models/models_20231219_ontology/d=mc_dtc2023_gd-drone-bird_o=gd-drone-bird_m=yolov8m_imgsz=5472_bs=1_20231219-054010",
+    "/scratch/cs/css-llm/jonathan/localcode/bb-yolo/models/models_20231219_ontology/d=mc_dtc2023_gd-shadow_o=gd-shadow_m=yolov8m_imgsz=5472_bs=1_20231219-054003",
+    "/scratch/cs/css-llm/jonathan/localcode/bb-yolo/models/models_20231219_ontology/d=mc_dtc2023_rm-unknown_o=rm-unknown_m=yolov8m_imgsz=5472_bs=1_20231219-054002",
+    # models_20231209_sc
+    "/scratch/cs/css-llm/jonathan/localcode/bb-yolo/models/models_20231209_sc/d=sc_dtc2023_m=yolov8n_imgsz=2560_bs=4_20231209-013849",
+    "/scratch/cs/css-llm/jonathan/localcode/bb-yolo/models/models_20231209_sc/d=sc_dtc2023_m=yolov8x_imgsz=2560_bs=4_20231209-013749",
+    "/scratch/cs/css-llm/jonathan/localcode/bb-yolo/models/models_20231209_sc/d=sc_dtc2023_m=yolov8n_imgsz=1280_bs=16_20231209-022311",
+    "/scratch/cs/css-llm/jonathan/localcode/bb-yolo/models/models_20231209_sc/d=sc_dtc2023_m=yolov8m_imgsz=1280_bs=16_20231209-013850",
+    "/scratch/cs/css-llm/jonathan/localcode/bb-yolo/models/models_20231209_sc/d=sc_dtc2023_m=yolov8x_imgsz=1280_bs=16_20231209-013850",
+    "/scratch/cs/css-llm/jonathan/localcode/bb-yolo/models/models_20231209_sc/d=sc_dtc2023_m=yolov8m_imgsz=5472_bs=1_20231209-013749",
 ]
 
+model_paths = [os.path.join(p, 'weights', 'best.pt') for p in model_paths]
 model_paths += [model_path.replace('best.pt', 'last.pt') for model_path in model_paths]
-print(f"{model_paths=}")
+newline = '\n'
+print(f"model_paths:\n{newline.join(model_paths)}")
 eval_results = []
 eval_dt = cur_dt_str()
 for model_path in model_paths:
     model = YOLO(model_path)
+    print(f'load model from {model_path}')
     print(f"{model.names=}")
 
     # set batch size
@@ -56,12 +57,12 @@ for model_path in model_paths:
     cls_map50_95 = metrics.box.maps
     cls_ap50 = metrics.box.ap50
     model_eval_results = {'model_path': model_path, 'datetime': cur_dt_str()}
-    model_eval_results |= {
+    model_eval_results.update({
         f"{model.names[cls]}_map": map for cls, map in zip(model.names, cls_map50_95)
-    }
-    model_eval_results |= {
+    })
+    model_eval_results.update({
         f"{model.names[cls]}_ap50": ap50 for cls, ap50 in zip(model.names, cls_ap50)
-    }
+    })
     print(f"{model_eval_results=}")
     eval_results.append(model_eval_results)
 
