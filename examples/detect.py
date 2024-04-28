65115e19d825e32c21ead007bb82be6dc9e2eeaa
Joilence <14588641+Joilence@users.noreply.github.com>
2024-04-28 22:52:03 +0200

    [add] example script: detect and batch track

--- examples/detect.py
+++ examples/detect.py
@@ -2,22 +2,34 @@ from pathlib import Path
 
 from bbtrack_yolo.BBDetecor import BBDetector, BBDetectorConfig
 
-test_video_path = Path("../tests/data/0190_0.5s.mp4")
-test_model_path = Path(
-    "../tests/data/d=mc_dtc2023_m=yolov8n_imgsz=1280_bs=16_20231118-014426/weights/best.pt"
+model_path = Path(
+    "/abyss/home/localcode/bb-yolo/"
+    "models/models_20231219_ontology/"
+    "d=mc_dtc2023_gd-shadow_o=gd-shadow_m=yolov8m_imgsz=5472_bs=1_"
+    "20231219-054003/"
+    "weights/best.pt"
 )
 
-assert test_video_path.exists(), "test_video_path does not exist"
-assert test_model_path.exists(), "test_model_path does not exist"
+video_dir = Path("data/videos")
+video_paths = list(video_dir.glob("*.[mM][pP]4"))
+
+print("Found video files:")
+print("\n".join([str(vp) for vp in video_paths]))
 
 # create detector config, "model" is required, other fields are optional
-detector_config = BBDetectorConfig(model=test_model_path)
+detector_config = BBDetectorConfig(model=model_path)
+print(f"{detector_config=}")
 
 # initialize detector
 detector = BBDetector(config=detector_config)
+print(f"{detector=}")
 
-# get detections
-dets = detector.detect(test_video_path)
+# iterate over video files
+for vp in video_paths:
+    print(f"processing {vp=}")
 
-# plot predictions on video
-dets.plot_on(test_video_path)
+    # get detections
+    dets = detector.detect(vp)
+
+    # plot predictions on video
+    # dets.plot_on(vp)
