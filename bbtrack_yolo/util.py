2406e0fc2673c70e28384e72b9262a0aaa3f19f5
Joilence <14588641+Joilence@users.noreply.github.com>
2024-05-16 18:36:21 +0200

    [fix] return dict.update() leads to None

--- bbtrack_yolo/util.py
+++ bbtrack_yolo/util.py
@@ -31,8 +31,8 @@ def get_default_tqdm_args(
     additional_args: Optional[Dict[str, Any]] = None
 ) -> Dict[str, Any]:
     """Get default tqdm arguments"""
-    return (
-        {
-            "bar_format": "{l_bar}{bar:10}{r_bar}",
-        }
-    ).update(additional_args or {})
+    default = {
+        "bar_format": "{l_bar}{bar:10}{r_bar}",
+    }
+    default.update(additional_args or {})
+    return default
