6c2469088447f56dc469770a6ff4d23dcebc28ca
Joilence <14588641+Joilence@users.noreply.github.com>
2024-06-19 16:42:42 +0200

    [refactor] format

--- yolov8_train.py
+++ yolov8_train.py
@@ -22,30 +22,48 @@ try:
     # add date as tag
     experiment.add_tag(f'train_{cur_dt_str().split("-", 1)[0]}')
 except ImportError:
-    print('comet_ml not installed, model training not logged.')
+    print("comet_ml not installed, model training not logged.")
     experiment = None
 except Exception:
-    print(f'model training not logged since comet failed to initialize.\n'
-          f'{COMET_API_KEY=}, {COMET_PROJECT_NAME=}, {COMET_WORKSPACE=}')
-    # TODO: give detail why comet failed to initialize. check where is .comet.config file
+    print(
+        f"model training not logged since comet failed to initialize.\n"
+        f"{COMET_API_KEY=}, {COMET_PROJECT_NAME=}, {COMET_WORKSPACE=}"
+    )
+    # TODO: give detail why comet failed to initialize. Look for `.comet.config`
     experiment = None
 finally:
     import torch
     from ultralytics import YOLO
-    from ultralytics import settings
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-m', '--model', type=str, default='yolov8m.pt', help='Model.pt path(s) or pretrained model name')
-parser.add_argument('-d', '--dataset', type=str,
-                    default='/abyss/home/localcode/labelbox-handler/yolov8_labels/mc_dtc2023/mc_dtc2023.yaml',
-                    help='dataset.yaml path')
-parser.add_argument('--image_size', type=int, default=5472,
-                    help='image size, default to original size in blackbuck dataset, 5472')
-parser.add_argument('--batch_size', type=int, default=1, help='batch size, default to auto (-1)')
-parser.add_argument('-e', '--epochs', type=int, default=10000, help='number of epochs, default to 10')
-parser.add_argument('--workers', type=int, default=8, help='number of workers for dataloader, default to 8')
-parser.add_argument('--patience', type=int, default=1000, help='early stopping patience, default to 100')
-parser.add_argument('--ontology', type=str, default='all-mc', help='str to indicate ontology, default to empty str')
+parser.add_argument(
+    "-m",
+    "--model",
+    type=str,
+    default="yolov8m.pt",
+    help="Model.pt path(s) or pretrained model name",
+)
+parser.add_argument(
+    "-d",
+    "--dataset",
+    type=str,
+    default="/abyss/home/localcode/labelbox-handler/yolov8_labels/mc_dtc2023/mc_dtc2023.yaml",
+    help="dataset.yaml path",
+)
+parser.add_argument(
+    "--image_size", type=int, default=5472, help="image size for training"
+)
+parser.add_argument("--batch_size", type=int, default=-1, help="batch size")
+parser.add_argument("-e", "--epochs", type=int, default=5000, help="number of epochs")
+parser.add_argument(
+    "--workers",
+    type=int,
+    default=8,
+    help="number of workers for dataloader, default to 8",
+)
+parser.add_argument(
+    "--patience", type=int, default=1000, help="early stopping patience"
+)
 
 args = parser.parse_args()
 print(args_in_lines(args))
@@ -56,42 +74,29 @@ epochs = args.epochs
 workers = args.workers
 base_model = args.model
 patience = args.patience
-ontology = args.ontology
-
-if ontology:
-    dataset_name = Path(dataset_file).stem
-    dataset_file = dataset_file.replace(dataset_name, f'{dataset_name}_{ontology}')
-    print(f"Ontology specified: {dataset_file=}")
-    assert Path(dataset_file).exists(), f"{dataset_file} does not exist!"
-
-# TODO: check if this is necessary
-settings.update({
-    'runs_dir': './runs',
-    'weights_dir': './models',
-})
 
 # if mps is available, indicate running on mac and no cuda available, use mps
-device = 'mps' if torch.backends.mps.is_available() else '0'
-if batch_size == -1 and device == 'mps':
+device = "mps" if torch.backends.mps.is_available() else "0"
+if batch_size == -1 and device == "mps":
     batch_size = 1  # mps does not support auto-batch size
 
 # modify dataset absolute path in .yaml file according to machine
-dataset_cfg = yaml.load(open(dataset_file, 'r'), Loader=yaml.FullLoader)
-dataset_cfg['path'] = Path(dataset_file).parent.absolute().as_posix()
-yaml.dump(dataset_cfg, open(dataset_file, 'w'))
+dataset_cfg = yaml.load(open(dataset_file, "r"), Loader=yaml.FullLoader)
+dataset_cfg["path"] = Path(dataset_file).parent.absolute().as_posix()
+yaml.dump(dataset_cfg, open(dataset_file, "w"))
 
 # load a model
 model = YOLO(base_model)
 
 model_train_param_str = f"m={Path(base_model).stem}_imgsz={image_size}_bs={batch_size}"
-model_name = f"d={Path(dataset_file).stem}_o={ontology}_{model_train_param_str}_{cur_dt_str()}"
+model_name = f"d={Path(dataset_file).stem}_{model_train_param_str}_{cur_dt_str()}"
 if experiment is not None:
     experiment.set_name(model_name)
 
 # train the model
 model.train(
     data=dataset_file,
-    optimizer='auto',
+    optimizer="auto",
     device=device,
     epochs=epochs,
     patience=patience,  # early stopping patience
@@ -103,7 +108,7 @@ model.train(
     imgsz=image_size,
     val=True,  # validate during training
     # save to project/name
-    project='models',
+    project="models",
     name=model_name,
 )
 
@@ -112,37 +117,36 @@ metrics = model.val(
     # data=, # need to specify dataset.yaml if not in default location
     imgsz=image_size,
     batch=1,
-    conf=0.001,  # default
-    iou=0.6,  # default
-    max_det=500,  # default
+    conf=0.001,  # default from YOLOv8
+    iou=0.6,  # default from YOLOv8
+    max_det=500,  # default from YOLOv8
     device=device,
     # save to project/name
-    project=f'models/{model_name}',
-    name='validation',
+    project=f"models/{model_name}",
+    name="validation",
 )  # returns: ultralytics.utils.metrics.Metric
 print(f"{metrics.box.maps=}")  # a list contains map50-95 of each category
 cls_map50_95 = metrics.box.maps
 cls_ap50 = metrics.box.ap50
 model_eval_results = {
-    'model_name': model_name,
-    'datetime': cur_dt_str(),
-    'ontology': ontology,
-    'cls_names': ','.join(model.names.values()),
-    'model': base_model,
-    'dataset': dataset_file,
-    'image_size': image_size,
-    'batch_size': batch_size,
+    "model_name": model_name,
+    "datetime": cur_dt_str(),
+    "cls_names": ",".join(model.names.values()),
+    "model": base_model,
+    "dataset": dataset_file,
+    "image_size": image_size,
+    "batch_size": batch_size,
 }
-model_eval_results.update({
-    f"{model.names[cls]}_map": map for cls, map in zip(model.names, cls_map50_95)
-})
-model_eval_results.update({
-    f"{model.names[cls]}_ap50": ap50 for cls, ap50 in zip(model.names, cls_ap50)
-})
+model_eval_results.update(
+    {f"{model.names[cls]}_map": map for cls, map in zip(model.names, cls_map50_95)}
+)
+model_eval_results.update(
+    {f"{model.names[cls]}_ap50": ap50 for cls, ap50 in zip(model.names, cls_ap50)}
+)
 print(f"{model_eval_results=}")
 
 # dump as json
-with open(f'models/{model_name}/processed_eval.json', 'w') as f:
+with open(f"models/{model_name}/processed_eval.json", "w") as f:
     json.dump(model_eval_results, f)
 
 print("Done!")
