f9af22036719db8ca0707b400f731b5e486e86d1
Joilence <14588641+Joilence@users.noreply.github.com>
2024-08-01 15:03:35 +0200

    [chore] add more examples in basic track eg

--- examples/basics/basic_track_and_plot.py
+++ examples/basics/basic_track_and_plot.py
@@ -8,6 +8,25 @@ all_dets = BBoxDetection.load_from("predictions/detection_190_5s.parquet")
 
 # initialize tracker
 tracker = BBoxTracker(tracker=OCSORT())
+# BBoxTracker(BYTETracker())
+# BBoxTracker(
+#     BoTSORT(
+#         model_weights=None,
+#         device=get_device(),
+#         fp16=False,
+#         with_reid=False,
+#     ),
+# )
+# BBoxTracker(
+#     BoTSORT(
+#         model_weights="resnet50_msmt17.pt",
+#         # more re-id models see:
+#         # https://kaiyangzhou.github.io/deep-person-reid/MODEL_ZOO.html
+#         device=get_device(),
+#         fp16=False,
+#         with_reid=False,
+#     ),
+# )
 
 # track detections
 trks = tracker.track(all_dets)
